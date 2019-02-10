d6d3f9fb03c5e73eb13aaa5a204b6e475708117c
Юлия Епанчинцева <i.epanchintseva@g.nsu.ru>
2019-02-10 20:19:27 +0700

    lexer added

--- src/ru/nsu/ccfit/epanchintseva/mtk/calculator/ParserException.java
+++
@@ -1,7 +0,0 @@
-package ru.nsu.ccfit.epanchintseva.mtk.calculator;
-
-public class ParserException extends Exception {
-    ParserException() {
-        System.out.println("Parser exception");
-    }
-}
